42bd5d0d027eec204fbbf5b67fc2f0c857ba12a3
bish boria <bishboria@gmail.com>
2011-03-13 10:34:09 +0000

    calculating the neighbours of a cell correctly for any cell

--- lib/2011_03_12_game_of_life.rb
+++ lib/2011_03_12_game_of_life.rb
@@ -1,5 +1,9 @@
 def neighbours_of row, column
-  [[0,0],[0,1],[0,2],
-  [1,0],[1,2],
-  [2,0],[2,1],[2,2]]
+  neighbours = []
+  (row-1).upto(row+1) do |r|
+    (column-1).upto(column+1) do |c|
+      neighbours << [r,c]
+    end
+  end
+  neighbours - [[row,column]]
 end
--- spec/2011_03_12_game_of_life_spec.rb
+++ spec/2011_03_12_game_of_life_spec.rb
@@ -28,5 +28,10 @@ describe "Game of Life" do
         neighbours_of(1,1).should include([2,2])
       end
     end
+    context "a cell located at 1,2" do
+      it "has neighbours located in the 8 cells surrounding it" do
+        neighbours_of(1,2).should == [[0,1],[0,2],[0,3],[1,1],[1,3],[2,1],[2,2],[2,3]]
+      end
+    end
   end
 end
