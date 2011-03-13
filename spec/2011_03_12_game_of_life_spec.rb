b5d98d9a1d16d62a0ea0c35ec4cb7a35ad1cf45f
bish boria <bishboria@gmail.com>
2011-03-13 10:25:23 +0000

    calculating the neighbours of 1,1

--- spec/2011_03_12_game_of_life_spec.rb
+++ spec/2011_03_12_game_of_life_spec.rb
@@ -1,1 +1,32 @@
 require 'spec_helper'
+
+describe "Game of Life" do
+  context "Working out neighbours" do
+    context "a cell located at position 1,1" do
+      it "has a neighbour at 0,0" do
+        neighbours_of(1,1).should include([0,0])
+      end
+      it "has a neighbour at 0,1" do
+        neighbours_of(1,1).should include([0,1])
+      end
+      it "has a neighbour at 0,2" do
+        neighbours_of(1,1).should include([0,2])
+      end
+      it "has a neighbour at 1,0" do
+        neighbours_of(1,1).should include([1,0])
+      end
+      it "has a neighbour at 1,2" do
+        neighbours_of(1,1).should include([1,2])
+      end
+      it "has a neighbour at 2,0" do
+        neighbours_of(1,1).should include([2,0])
+      end
+      it "has a neighbour at 2,1" do
+        neighbours_of(1,1).should include([2,1])
+      end
+      it "has a neighbour at 2,2" do
+        neighbours_of(1,1).should include([2,2])
+      end
+    end
+  end
+end
